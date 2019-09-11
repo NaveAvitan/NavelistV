12431ffc9c945a371c294d5efbdd201e81047e87
nave <na1212@amaknet.k12.il>
2019-09-11 14:29:54 +0300

    tbc

--- app/src/main/java/com/example/navelistv/MainActivity.java
+++ app/src/main/java/com/example/navelistv/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.navelistv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ListView lv;
+    String [] yabeshet= {"Europe","America","Asia","Africa"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv= (ListView) findViewById(R.id.mylv);
+        lv.setOnClickListener(this);
+        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+        ArrayAdapter<String> adp= new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,yabeshet);
+        lv.setAdapter(adp);
+    }
+
+
+
+
+    @Override
+    public void  {
+
     }
 }
