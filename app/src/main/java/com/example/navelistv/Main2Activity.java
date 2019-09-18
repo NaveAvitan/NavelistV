995107bcf34731ad8f8a9093a98eed7bf9b66da6
user <na1212@bs.amalnet.k12.il>
2019-09-18 14:30:51 +0300

    tca

--- app/src/main/java/com/example/navelistv/Main2Activity.java
+++ app/src/main/java/com/example/navelistv/Main2Activity.java
@@ -1,13 +1,32 @@
 package com.example.navelistv;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class Main2Activity extends AppCompatActivity {
-
+public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    Intent si= getIntent();
+    int x= si.getIntExtra("n",1);
+    ListView lv2;
+    String[] Europe = {"Israel", "Spain", "Italy", "France","Sweden","Germany","England"};
+    String[] america = {"Brazil", "USA", "Mexico", "Canada","Argentina","Colombia","Peru"};
+    String[] asia = {"Japan", "China", "Thailand", "India","Singapore","Qatar","Lebanon"};
+    String[] africa = {"Nigeria", "Zanzibar", "Egypt", "Morocco","Algeria","Senegal","Gabon"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        lv2 = (ListView) findViewById(R.id.mylv);
+        lv2.setOnClickListener((View.OnClickListener) this);
+        lv2.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
