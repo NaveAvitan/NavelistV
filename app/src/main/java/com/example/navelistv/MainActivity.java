11dcbff0332b6f42cf7f3a13f9d8b7f062542786
hp <na1212@bs.amalnet.k12.il>
2019-09-28 17:53:54 +0300

    kkk

--- app/src/main/java/com/example/navelistv/MainActivity.java
+++ app/src/main/java/com/example/navelistv/MainActivity.java
@@ -11,44 +11,45 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.Collections;
+
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     ListView lv;
-    String[] yabeshet = {"Europe", "America", "Asia", "Africa"};
+    String[] yab;
     String choice;
     int ch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.lv);
 
+
+
+
+        ArrayAdapter<String> adp1= new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, yab);
+
+        lv.setAdapter(adp1);
         lv.setOnItemClickListener(this);
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-
-        ArrayAdapter <String> adp= new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,yabeshet);
-        lv.setAdapter(adp);
-
-
-
-
     }
-
-
 
 
     public void neatest(View view) {
-        Intent gi;
-        gi = new Intent(this,Main2Activity.class);
-        Intent intent = gi.putExtra("n:", ch);
+        Intent gi = new Intent(this,Main2Activity.class);
+        gi.putExtra("n:", ch);
         startActivity(gi);
     }
 
+
+
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Toast toast;
-        toast=Toast.makeText(this, "" + yabeshet[position], Toast.LENGTH_SHORT);
+        toast=Toast.makeText(this, "" + yab[position], Toast.LENGTH_SHORT);
         ch= position;
         toast.show();
-    }
-}
+    } }
+
